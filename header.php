b53824f54139c1dea1b4475d68b8d68fe81e1d21
Muhammad Ulil Albab <ulilalbam@gmail.com>
2020-03-19 12:40:33 +0700

    remove unused search and setting left sidebar

--- header.php
+++ header.php
@@ -35,20 +35,10 @@
 <!-- Overlay For Sidebars -->
 <div class="overlay"></div>
 
-<!-- Main Search -->
-<div id="search">
-    <button id="close" type="button" class="close btn btn-primary btn-icon btn-icon-mini btn-round">x</button>
-    <form>
-      <input type="search" value="" placeholder="Search..." />
-      <button type="submit" class="btn btn-primary">Search</button>
-    </form>
-</div>
-
 <!-- Right Icon menu Sidebar -->
 <div class="navbar-right">
     <ul class="navbar-nav">
-        <li><a href="#search" class="main_search" title="Search..."><i class="zmdi zmdi-search"></i></a></li>
-        <li><a href="javascript:void(0);" class="js-right-sidebar" title="Setting"><i class="zmdi zmdi-settings zmdi-hc-spin"></i></a></li>
+        
         <li><a href="logout.php" class="mega-menu" title="Sign Out"><i class="zmdi zmdi-power"></i></a></li>
     </ul>
 </div>
